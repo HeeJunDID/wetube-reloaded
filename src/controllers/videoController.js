df6ad0eb8aca2677fc66fe2eb419eca388c4a89b
양희준 <yhj@yanghuijun-ui-MacBookAir.local>
2021-10-09 20:51:32 +0900

    iteration video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,20 @@
-export const trending = (req, res) => res.render("home", {pageTitle : "Home"});
-export const see = (req, res) => res.render("watch", {pageTitle : "Watch"});
-export const edit = (req,res) => res.render("edit", {pageTitle : "Edit"});
-export const search = (req,res) => res.send("Search");
-export const upload = (req,res) => res.send("Upload");
-export const deleteVideo = (req,res) => {
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "Hello",
+        },
+        {
+            title: "Video #2",
+        },
+        {
+            title: "whatsup",
+        }];
+    return res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
     return res.send("Delete Video")
 }
